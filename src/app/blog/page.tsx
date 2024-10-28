03fb402ad44830ec6627bb00c2b8e794bc749d0f
YolxanderHC <yolxander.jaca@humancontact.com>
2024-10-28 19:41:50 -0400

    updated code

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,12 @@
-
 import {FooterComponent} from "@/components/footer";
-
 import {Navbar} from "@/components/Navbar";
+import {TravelBlogPostComponent} from "@/components/blog/travel-blog-post";
 
 export default function HomePage() {
     return (
         <div>
             <Navbar/>
-
+            <TravelBlogPostComponent />
             <FooterComponent />
         </div>
     );
