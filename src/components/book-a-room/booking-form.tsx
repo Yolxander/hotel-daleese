91f463136f6c55169503458d8c62cb3749009f7b
YolxanderHC <yolxander.jaca@humancontact.com>
2024-10-29 11:09:32 -0400

    added booking page

--- src/components/book-a-room/booking-form.tsx
+++ src/components/book-a-room/booking-form.tsx
@@ -31,7 +31,7 @@ export function BookingFormComponent() {
   }
 
   return (
-    <section className={`bg-white text-black py-16 ${lora.className} pt-[250px]`}>
+    <section className={`bg-white text-black py-16 ${lora.className} md:pt-[250px] pt-[200px]`}>
       <div className="container mx-auto px-4 max-w-2xl">
         <h2 className="text-4xl  mb-6 text-center">Book a room</h2>
         <p className="mb-8 text-center">
