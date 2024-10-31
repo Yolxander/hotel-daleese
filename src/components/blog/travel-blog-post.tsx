bdfc33fb5b96a1623f44e5c0064daf48bad557c5
YolxanderHC <yolxander.jaca@humancontact.com>
2024-10-31 00:50:08 -0400

    update blog

--- src/components/blog/travel-blog-post.tsx
+++ src/components/blog/travel-blog-post.tsx
@@ -9,14 +9,14 @@ const cormorantGaramond = Cormorant_Garamond({ subsets: ['latin'], weight: ['300
 export function TravelBlogPostComponent() {
   return (
     <section className={`container mx-auto px-4 py-16 ${cormorantGaramond.className} md:mt-[200px] mt-[50px] md:h-[90vh] h-[110vh]`}>
-      <div className="flex flex-col md:flex-row gap-12">
+      <div className="flex flex-col md:flex-row md:gap-12">
         <div className="md:w-1/2">
           <Image
             src="https://storage.googleapis.com/sempre-studios-893c8.appspot.com/uploads/Casa%20Turul/Blog/671fd74a7475b.jpeg?GoogleAccessId=firebase-adminsdk-gkp49%40sempre-studios-893c8.iam.gserviceaccount.com&Expires=1761675979&Signature=iGbHxggQGFm%2BQLVSj2zSx6zsKy1lRv2CVumjbGVuSLksUS%2BEQm3KQWT52DubXbBibfaH2nLmhKXBakp3PWSEnRulz41VUcoqtNGnv6AHcwgLVaHDNVy16OVWgGrGb1fXn%2BBjsz0xmeuO7SrYZMEhwWyGVOjkRvLJnLMcXI5tiDM25BiYjWvHJrfI%2F92teRLhBD1G97VXdF6lTH%2FRVlkA%2FADVWqs1rf1GcVoUOdiwasp9sHoq5zn8Ei%2BJVvrfWDzBgJrBLXu4YYn2w9kgQlEQcZwYZ8%2BP1jfZHxqgbaxb6IFKKJZGx4q5Dyb%2FZKknLfjYXjRkAV7BvgnOzDXpuwwuug%3D%3D"
             alt="Person sitting on a car overlooking a mountain in Costa Rica"
             width={800}
             height={200}
-            className="w-full h-[560px] object-cover"
+            className="w-full md:h-[560px] h-[300px] object-cover"
           />
         </div>
         <div className="md:w-1/2 flex flex-col justify-center md:gap-8">
