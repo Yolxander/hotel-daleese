66d6b4808f9c845e28df52c491e5e7cf5f7761c9
YolxanderHC <yolxander.jaca@humancontact.com>
2024-11-12 10:11:32 -0500

    updated images and opacity

--- src/components/landing_page/hero-with-navbar.tsx
+++ src/components/landing_page/hero-with-navbar.tsx
@@ -18,7 +18,7 @@ export function HeroWithNavbarComponent() {
             justifyContent: "space-between",
         }}>
             <motion.div
-                className="absolute inset-0 bg-black bg-opacity-32 pointer-events-none "
+                className="absolute inset-0 bg-black bg-opacity-10 pointer-events-none "
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 1 }}
--- src/components/landing_page/hotel-home-section.tsx
+++ src/components/landing_page/hotel-home-section.tsx
@@ -25,7 +25,7 @@ export function HotelHomeSectionComponent() {
                 className="absolute inset-0"
             >
                 <Image
-                    src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/original-EEDB3FA3-872F-4F24-9BE0-04B7286A4481-dNnTtJWCzTvDp3jwIac8okma1qX9xX.jpeg"
+                    src="https://storage.googleapis.com/sempre-studios-893c8.appspot.com/uploads/67336fd10e763.jpg?GoogleAccessId=firebase-adminsdk-gkp49%40sempre-studios-893c8.iam.gserviceaccount.com&Expires=1762960210&Signature=TqR36aeJCJf9z5wXj%2BaQTRZhS4oDVzILqBuOZfOzWCS4hJSmiHox11aGHRrtB600seSp8s0LhterEEIsCYVLdV649sMWNk0SuVlFo8%2BYBFUo%2FT4vtOSImspNSuHUjmCqOi9WQoiy3%2B9%2Bc4r6fl6rJv0ab5ZGIrOCaxvpwx6%2B1WcIEERadp%2B3op71017z6K%2FcE3Y1Q4SLHSBd4heOcsWPqIlXfNgY3%2FgVZYcPZK8bEJnTyYU%2BMbMMo4%2F0UJqwtaENxR537zedKOP2GVIOMV9L7yLjwHN5KKpcmkqgeYH13E1pbYoR2HV%2FnQxrGLrd9MeLlXQm5nF87HnrRuQKDAHpTA%3D%3D"
                     alt="Tropical hotel pool area with lush vegetation and outdoor dining"
                     fill
                     style={{ objectFit: 'cover' }}
